b58d005df75702599b2a79708c4219d77d3e0e51
Azamshoukat003 <140690515+Azamshoukat003@users.noreply.github.com>
2023-10-22 08:39:14 -0700

    Add files via upload

---
+++ base/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=90)
+    email = models.EmailField(max_length=120)
+    password = models.CharField(max_length=90)
+    
+    def __str__(self):
+        return self.name
+    
